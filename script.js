20f59a7e8c1e9f0cad52830b866a4c74fedc17bf
thorellaetitia <laetis76@gmail.com>
2018-10-11 10:25:07 +0200

    fin exo 4

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById('submit').addEventListener('click',function (){
+  var lastname=document.getElementById('lastname').value="";
+  var firstname=document.getElementById('firstname').value="";
+  var city=document.getElementById('city').value="";
+(result=lastname+firstname+city);
+}
+
+)
+//var pas obligatoire car il y a l'id//
